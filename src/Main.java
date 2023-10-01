e611e8c4f0402301241a73c888b657701edbf4f1
Yuval Cohen <yuvalco1@gmail.com>
2023-10-01 23:44:21 +0300

    commit execrise hw2#6

--- src/Main.java
+++ src/Main.java
@@ -68,25 +68,33 @@ public class Main {
 
 //Exercise hw2#5
 
+//        Scanner scanner = new Scanner(System.in);
+//        int sum_odd = 0;
+//        int sum_even = 0;
+//        int counter = 1;
+//        while (counter < 11) {
+//            System.out.println("please enter number:");
+//            int input_number = scanner.nextInt();
+//            if (counter % 2 == 1) {
+//                sum_odd += input_number;
+//            } else {
+//                sum_even += input_number;
+//            }
+//            counter++;
+//        }
+//        System.out.println("sum of numbers in odd position is: " + sum_odd);
+//        System.out.println("sum of numbers in even position is: " + sum_even);
+
+//Exercise hw2#6
+
         Scanner scanner = new Scanner(System.in);
-        int sum_odd = 0;
-        int sum_even = 0;
-        int counter = 1;
-        while (counter < 11) {
-            System.out.println("please enter number:");
-            int input_number = scanner.nextInt();
-            if (counter % 2 == 1) {
-                sum_odd += input_number;
-            } else {
-                sum_even += input_number;
-            }
-            counter++;
+        System.out.println("please enter number n, to calculate sum of fraction 1+1/2+1/3+...+1/n:");
+        int n = scanner.nextInt();
+        double sum_fraction = 0;
+        for (int i = 1; i <= n; i++) {
+            sum_fraction += 1.0 / i;
         }
-        System.out.println("sum of numbers in odd position is: " + sum_odd);
-        System.out.println("sum of numbers in even position is: " + sum_even);
-
-
-
+        System.out.println("sum of fraction 1+1/2+1/3+...+1/"+n+" is: " + sum_fraction);
 
     }
 }
