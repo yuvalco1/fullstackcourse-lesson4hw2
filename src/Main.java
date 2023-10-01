ec1e954d2465db46dcc8e7a4ce8b1f94d611e2ca
Yuval Cohen <yuvalco1@gmail.com>
2023-10-01 23:09:09 +0300

    commit execrise hw2#2

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,24 @@ public class Main {
 //        System.out.println("sum of positive numbers is: " + sum);
 
 
-
-
+//Exercise hw2#
+// get 10 numbers from users and print sum of positive numbers and sum of negative numbers
+        Scanner scanner = new Scanner(System.in);
+        int sum_pos = 0;
+        int sum_neg = 0;
+        int counter = 0;
+        while (counter < 10) {
+            System.out.println("please enter number:");
+            int input_number = scanner.nextInt();
+            if (input_number > 0) {
+                sum_pos += input_number;
+            } else if (input_number < 0) {
+                sum_neg += input_number;
+            }
+            counter++;
+        }
+        System.out.println("sum of positive numbers is: " + sum_pos);
+        System.out.println("sum of negative numbers is: " + sum_neg);
 
     }
 }
