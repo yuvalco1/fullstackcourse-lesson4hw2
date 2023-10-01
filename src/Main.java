d59596f9730ea35872c0bf7b3b2d6779232e0482
Yuval Cohen <yuvalco1@gmail.com>
2023-10-01 23:02:34 +0300

    first commit- exercise hw2#1

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//Exercise hw2#1
+// get 10 numbers from users and print sum of positive numbers only
+
+//        Scanner scanner = new Scanner(System.in);
+//        int sum = 0;
+//        int counter = 0;
+//        while (counter < 10) {
+//            System.out.println("please enter number:");
+//            int input_number = scanner.nextInt();
+//            if (input_number > 0) {
+//                sum += input_number;
+//            }
+//            counter++;
+//        }
+//        System.out.println("sum of positive numbers is: " + sum);
+
+
+
+
+
+    }
+}
